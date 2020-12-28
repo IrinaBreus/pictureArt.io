ceaff6cd202fed2aa34ede7b4f59e6ca9b12a4b7
Irina <irabreus@gmail.com>
2020-12-28 10:41:51 +0300

    add pictureSize

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
 'use strict';
@@ -20,4 +21,5 @@ checkTextInputs('[name="message"]');
 showMoreStyles('.button-styles', '#styles .row');
 calc();
 filter();
+pictureSize('.sizes-block');
 });
